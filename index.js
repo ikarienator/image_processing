bee46d851aba45c5410f8196b584df7a3d4fb11e
Bei Zhang <ikarienator@google.com>
2013-08-19 17:01:03 -0700

    Fix bug

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ function init() {
   var gy = postproc.createProgram(loadShader('shaders/gaussian.y.fs.glsl'));
   var sobel = postproc.createProgram(loadShader('shaders/sobel.fs.glsl'));
   var copy = postproc.createProgram(loadShader('shaders/copy.fs.glsl'));
-  var origin = postproc.createInput(null, 1024, 1024);
+  var merger = postproc.createProgram(loadShader('shaders/merge.fs.glsl')); 
+  var origin = postproc.createInput(new Uint32Array(1024 * 1024), 1024, 1024);
 
   function tick() {
     srcCtx.drawImage(video, 0, 0);
     postproc.setupInputImage(srcCanvas);
     postproc.copyTo(origin);
     postproc.iterate(sobel);
+    postproc.callProgram(merger, [postproc.working_textures[1], origin], postproc.working_textures[0], {});
     postproc.render();
 
     webkitRequestAnimationFrame(tick);
